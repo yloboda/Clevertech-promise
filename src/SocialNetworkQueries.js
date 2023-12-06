7157fa5a7770ffc80460bed1c8b2ddf44bf61b0f
julia.loboda <julia.loboda@dataart.com>
2023-12-06 18:13:37 -0500

    Solution Iuliia Loboda

--- src/SocialNetworkQueries.js
+++ src/SocialNetworkQueries.js
@@ -2,10 +2,51 @@ export class SocialNetworkQueries {
 
     constructor({ fetchCurrentUser }) {
         this.fetchCurrentUser = fetchCurrentUser;
+        this.lastData = null;
     }
 
-    async findPotentialLikes(minimalScore) {
-        // TODO: IMPLEMENT HERE
+    findPotentialLikes(minimalScores) {
+        return this.fetchCurrentUser()
+            .then(userData => {
+                this.lastData = userData
+                return this.calculatePotentialLikes(userData, minimalScores);
+            })
+            .catch(() => {
+                if (this.lastData) {
+                    return this.calculatePotentialLikes(this.lastData, minimalScores);
+                } else {
+                    return [];
+                }
+            });
     }
 
+    calculatePotentialLikes(userData, minimalScores) {
+        if (!userData || !userData.likes || !userData.friends) {
+            return [];
+        }
+
+        const userLikes = new Set(userData.likes);
+        const bookScores = {};
+        const friendsCount = userData.friends.length;
+
+        userData.friends.forEach(friend => {
+            if (friend && friend.likes) {
+                new Set(friend.likes).forEach(book => {
+                    if (!userLikes.has(book)) {
+                        bookScores[book] = (bookScores[book] || 0) + 1;
+                    }
+                });
+            }
+        });
+
+        let potentialLikes = Object.keys(bookScores)
+            .filter(book => (bookScores[book] / friendsCount) >= minimalScores)
+            .sort((a, b) => {
+                const countDiff = bookScores[b] - bookScores[a];
+                if (countDiff !== 0) return countDiff;
+                return a.localeCompare(b);
+            });
+
+        return potentialLikes;
+    }
 }
